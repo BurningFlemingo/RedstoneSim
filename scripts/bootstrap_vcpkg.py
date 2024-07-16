6c992057e5e48a2ccee3835c2a74cb264ed19b05
FlemingJR <flemingjohnrobert.jrf@gmail.com>
2024-07-15 21:37:01 -0400

    setup

---
+++ scripts/bootstrap_vcpkg.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+import platform
+
+rootDir = os.getcwd()
+
+def buildPathFromRoot(*subdirs):
+    return os.path.join(rootDir, *subdirs)
+
+
+if __name__ == "__main__":
+    buildDir = buildPathFromRoot("build")
+    vcpkgDir = buildPathFromRoot("vendor", "vcpkg")
+
+    os.makedirs(buildDir, exist_ok=True)
+    os.makedirs(vcpkgDir, exist_ok=True)
+
+    platform = platform.system().lower();
+    if platform == "linux":
+        vcpkgBootstrap = f"{buildPathFromRoot(vcpkgDir, 'bootstrap-vcpkg.sh')}"
+    elif platform == "windows":
+        vcpkgBootstrap = f"{buildPathFromRoot(vcpkgDir, 'bootstrap-vcpkg.bat')}"
+    else:
+        print(f"Unsupported platform: {platform}")
+        exit()
+
+    subprocess.run([vcpkgBootstrap])
